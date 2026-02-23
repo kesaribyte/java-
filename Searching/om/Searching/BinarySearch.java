11e8af4cf159e64d0b7cee5e3f712f731cdbe7b9
omraje shitole <shitoleomraje12@gmail.com>
2026-02-23 16:10:03 +0530

    add code

--- Searching/om/Searching/BinarySearch.java
+++ Searching/om/Searching/BinarySearch.java
@@ -1,5 +1,36 @@
 package Searching;
 
-public class binary-search {
-    
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {-23, 2, 4, 5, 7, 8, 9, 34, 56, 87, 90};
+        int target = 87;
+
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
 }
